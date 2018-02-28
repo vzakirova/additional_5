945c676095b0db627108d832c7110c420562ad75
vzakirova <veranika.zakirava@gmail.com>
2018-03-01 01:10:46 +0300

    not all tests but i need to save at the point

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+
+  unev = str.length % 2;
+  if (unev != 0) return false; //если скобок нечетное число, соответствие невозможно
+
+  var arr = [];
+
+  for (i = 0; i < str.length; i++)
+  {
+    for (j = 0; j < bracketsConfig.length; j++)
+    {
+      if (str[i] == bracketsConfig[j][0]) 
+        arr.push(str[i]);
+
+      else if (str[i] == bracketsConfig[j][1] && arr[arr.length - 1] == bracketsConfig[j][0])
+          arr.pop();
+    }
+  }
+
+  if (arr.length === 0)
+    checkRes = true
+  else checkRes = false
+   
+  return checkRes
 }
